8d85777d47b971299f461465358eb96bda2785bc
Richard Price <richyprice77@gmail.com>
2022-01-25 21:46:27 +0000

    initial commit

---
+++ SwiftUIWeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWeSplit
+//
+//  Created by Richard Price on 25/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWeSplit/SwiftUIWeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeSplitApp.swift
+//  SwiftUIWeSplit
+//
+//  Created by Richard Price on 25/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
