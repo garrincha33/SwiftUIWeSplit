c6fecf6be9ad4e547bfdb19bc5333679b7291ffc
Richard Price <richyprice77@gmail.com>
2022-01-26 06:32:16 +0000

    added some state bindings to store amount number of people and tip percentage

--- SwiftUIWeSplit/ContentView.swift
+++ SwiftUIWeSplit/ContentView.swift
@@ -4,53 +4,30 @@
 //
 //  Created by Richard Price on 25/01/2022.
 //
-
-///Key points
-///@State is a 2 way binding it reads and writes values
-///use this with say a text field with the $0 then drop
-///it to actually print the value
-///if local, state shoulld be marked private
-///the reason we have state is because you cant modify structs, you can use a class as these are reference types
-///but keeping things to structs where you can improves performance
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
     
-    
-    let students = ["Rich", "Wayne", "Gerb"]
-    @State private var selectedEmployee = "Rich"
-    @State private var count = 0
-    @State private var name = ""
-   
-   
     var body: some View {
-        NavigationView {
-            Form {
-                Picker("Select your poison", selection: $selectedEmployee) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-                
-                Section {
-                    Text("Button Tapper")
-                    Button("tap count: \(count)") {
-                        self.count += 1
-                    }
-                }
-                
-                Section {
-                    TextField("Enter your name", text: $name)
-                    Text("your name is :- \(name)")
-                }
-            }.navigationTitle("Testing State")
+        
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
         }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
